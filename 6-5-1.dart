116e4b0165bdc26046c840f1545151e0765ac1b6
Mannpandya27 <132265675+Mannpandya27@users.noreply.github.com>
2024-03-20 00:38:42 +0530

    Add files via upload

---
+++ 6-5-1.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+void main() {
+  int a, b;
+
+  stdout.write("Enter A:");
+  a = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter B:");
+  b = int.parse(stdin.readLineSync()!);
+
+  try {
+    print("Ans: ${a ~/ b}");
+  } on IntegerDivisionByZeroException {
+    print("Can Not Divided By Zero");
+  }
+}
